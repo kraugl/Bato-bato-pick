ad4174b5fbc77daf95d71870de3b7f05e777bdb1
edou <edouardjade01@gmail.com>
2022-09-30 14:25:45 +0800

    Add playRound function

--- index.js
+++ index.js
@@ -11,3 +11,23 @@ function getComputerChoice() {
     }
 };
 
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+    if((playerSelection.toLowerCase() === 'rock') && (computerSelection === 'paper')){
+        return 'You Lose!!! Paper beats rock';
+    } else if((playerSelection.toLowerCase() === 'paper') && (computerSelection === 'scissors')){
+        return 'You Lose!!! Scissors beat paper';
+    } else if((playerSelection.toLowerCase() === 'scissors') && (computerSelection === 'rock')){
+        return 'You Lose!!! Rock beats scissors';
+    } else if((playerSelection.toLowerCase() === 'rock') && (computerSelection === 'scissors')){
+        return 'You Won!!! Rock beats scissors';
+    } else if((playerSelection.toLowerCase() === 'paper') && (computerSelection === 'rock')){
+        return 'You Won!!! Paper beats rock';
+    } else if((playerSelection.toLowerCase() === 'scissors') && (computerSelection === 'paper')){
+        return 'You Won!!! Scissors beat paper';
+    } else if((playerSelection.toLowerCase() === computerSelection)){
+        return 'It\'s draw!!!';
+    } else {
+        return 'Choose between rock, paper, and scissors';
+    }
+  };
