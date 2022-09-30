c9f81c226174f45e5bb3fb3fd88d003df23cba89
edou <edouardjade01@gmail.com>
2022-09-30 14:25:02 +0800

    Add getComputerChoice function

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 let choiceChecker = Math.floor(Math.random() * 100) + 1;
 
+function getComputerChoice() {
+
+    if(choiceChecker <= 33) {
+        return 'rock';
+    } else if(choiceChecker >= 66) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+};
+
